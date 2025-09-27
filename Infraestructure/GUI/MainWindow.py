189790b5497632d743b487c373ba16ae10e8b5c8
felipedelosh <doctorfhernandez@hotmail.com>
2025-09-27 17:34:36 -0500

    WIP:GUI

--- Infraestructure/GUI/MainWindow.py
+++ Infraestructure/GUI/MainWindow.py
@@ -19,6 +19,7 @@ class MainWindow:
         self.controller = controller
         self.root = tk.Tk()
         self.sideBar = tk.Canvas(self.root, bg="red", highlightthickness=0)
+        self.sideBarButtons = []
         self._content_x = 0
         self.content = tk.Canvas(self.root, bg="blue", highlightthickness=0)
         self.manager = ScreenManager(self.root, self.sideBar, self.content, self.controller)
@@ -50,13 +51,25 @@ class MainWindow:
         self.sideBar["height"] = self.controller.dependencies["config"].get("window_h")
         self.sideBar.place(x=0, y=0)
 
+        _counter_btn = 0
+        _x0 = int(self.sideBar["width"]) * 0.1
+        _btnW = int(self.sideBar["width"]) * 0.8
+        _dy = self.controller.dependencies["config"].get("window_h") / len(self.manager._screen_classes)
+        for itterOption in self.manager._screen_classes:
+            btn = tk.Button(self.sideBar, text=itterOption, command=lambda opt=itterOption: self.show_view(opt))
+            self.sideBarButtons.append(btn)
+            self.sideBarButtons[_counter_btn].place(x=_x0, y=(_dy * (_counter_btn)) + 10, width=_btnW)
+
+            _counter_btn = _counter_btn + 1
+
+
     def _configureContent(self):
         self._content_x = self.controller.dependencies["config"].get("window_w") * 0.3
         self.content["width"] = self.controller.dependencies["config"].get("window_w") - self._content_x
         self.content["height"] = self.controller.dependencies["config"].get("window_h")
 
-    def show_view(self, name):
-        pass
+    def show_view(self, option):
+        print(option)
 
     def run(self):
         self.root.mainloop()
