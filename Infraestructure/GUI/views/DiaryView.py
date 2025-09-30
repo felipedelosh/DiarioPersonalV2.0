889f406800229295d0a54080ce1bd765c9dc7ff1
felipedelosh <doctorfhernandez@hotmail.com>
2025-09-30 12:56:31 -0500

    WIP:GUI

--- Infraestructure/GUI/views/DiaryView.py
+++ Infraestructure/GUI/views/DiaryView.py
@@ -2,6 +2,7 @@
 FelipedelosH
 2025
 """
+import tkinter as tk
 from Infraestructure.GUI.Screen import Screen
 
 class DiaryView(Screen):
@@ -10,8 +11,26 @@ class DiaryView(Screen):
         self.canvas.place(x=x, y=y)
         _w = float(self.canvas["width"])
         _h =float(self.canvas["height"])
-
         lang = self.manager.controller.dependencies["lang"]
+        self.btns = []
+        _options = lang.getText("diary_options")
+        lblTitle = tk.Label(self.canvas, text=lang.getText("diary_title_message"))
+        lblTitle.place(x=_w*0.4, y=_h*0.05)
+        self.renderButons(_options, _w, _h)
 
-    def renderButons(self):
-        pass
+    def renderButons(self, _options, _w, _h):
+        _total_butons = len(_options)
+        if _total_butons > 0:
+            _total_btns_w = 0
+            for i in _options:
+                btn = tk.Button(self.canvas, text=i)
+                _total_btns_w = _total_btns_w + btn.winfo_reqwidth()
+                self.btns.append(btn)
+
+            _space_free = _w - _total_btns_w
+            _spacing = _space_free / (_total_butons + 1)
+            current_x = _spacing
+            for btn in self.btns:
+                btn.place(x=current_x, y=_h * 0.2)
+                current_x += btn.winfo_reqwidth() + _spacing
+
--- Infraestructure/GUI/views/IndexView.py
+++ Infraestructure/GUI/views/IndexView.py
@@ -14,14 +14,14 @@ class IndexView(Screen):
 
         lang = self.manager.controller.dependencies["lang"]
         time_util = self.manager.controller.utils["time_util"]
-        lblTitle = tk.Label(self.canvas, text=lang.getText("welcome_message"))
+        lblTitle = tk.Label(self.canvas, text=lang.getText("welcome_title_message"))
         lblTitle.place(x=_w*0.4, y=_h*0.05)
         _currentDayName = lang.getText("days_names")[time_util.getNumberOfCurrentDD()]
         _currentMonthName = lang.getText("month_names")[time_util.getCurrentMM()-1]
         _txtDateToday = f"{_currentDayName}, {time_util.getCurrentDD()} - {_currentMonthName} - {time_util.getCurrentYYYY()}".upper()
         lblCurrentDate = tk.Label(self.canvas, text=_txtDateToday)
-        lblCurrentDate.place(x=_w*0.1, y=_h*0.15)
+        lblCurrentDate.place(x=_w*0.05, y=_h*0.15)
         _motivational_message = random.choice(lang.getText("motivational_message"))
         lblMainMessage = tk.Label(self.canvas, text=_motivational_message)
-        lblMainMessage.place(x=_w*0.1, y=_h*0.2)
+        lblMainMessage.place(x=_w*0.05, y=_h*0.2)
         lblLastUsages = tk.Label(self.canvas, text="Esto casi no se usa.")
