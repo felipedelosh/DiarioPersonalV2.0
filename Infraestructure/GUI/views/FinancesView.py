755a607286670b847defb873d9b4d759800c8333
felipedelosh <doctorfhernandez@hotmail.com>
2025-10-19 18:04:22 -0500

    Economy

--- Infraestructure/GUI/views/FinancesView.py
+++ Infraestructure/GUI/views/FinancesView.py
@@ -8,7 +8,13 @@ class FinancesView(Screen):
     def render(self, x, y):
         self.canvas["bg"]="yellow"
         self.canvas.place(x=x, y=y)
-        _w = float(self.canvas["width"])
-        _h =float(self.canvas["height"])
+        self._w = float(self.canvas["width"])
+        self._h =float(self.canvas["height"])
+        self.lang = self.manager.controller.dependencies["lang"]
+        self.usageService = self.manager.controller.dependencies["usage_service"]
+        _options = self.lang.getText("economy_options")
 
-        lang = self.manager.controller.dependencies["lang"]
+        self.renderButons(_options, self._w, self._h)
+
+    def renderButons(self, _options, _w, _h):
+        pass
